d45c2fa9e970014631b2e171d2e3c707d23ac0b6
Anjana <anjanaanjumk@gmail.com>
2023-06-16 08:49:57 +0530

    Add Initial Structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './asserts/css/index.css';
+import Home from './pages/portfolio/home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
+
